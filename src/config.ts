92e56b165acc17ecf19c7515af1139088a88275a
Chris Judd <85649796+juddisjudd@users.noreply.github.com>
2024-01-22 16:19:33 -0800

    bigger init

--- src/config.ts
+++ src/config.ts
@@ -3,19 +3,22 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
 const DISCORD_TOKEN = process.env.TOKEN;
 
-if (!DISCORD_TOKEN || !CLIENT_ID ) {
+if (!DISCORD_TOKEN || !CLIENT_ID || !GUILD_ID) {
   throw new Error('Missing environment variables');
 }
 
 interface Env {
   DISCORD_TOKEN: string;
   CLIENT_ID: string;
+  GUILD_ID: string;
 }
 
 const Config: Env = {
   CLIENT_ID,
+  GUILD_ID,
   DISCORD_TOKEN,
 };
 
